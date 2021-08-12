a9db3d611d4fec745db0111c047242188ad339e2
Ariel29123 <73623027+Ariel29123@users.noreply.github.com>
2021-08-12 11:10:55 -0300

    Create Ej14AlgCon.c

---
+++ Ej14AlgCon.c
@@ -0,0 +1,12 @@
+#include <stlib.h>
+#include <stdio.h>
+
+int  main(argc, char *argv[]) {
+  int max = INT_MIN;
+  for (int i = 1; i < argc; i++){
+    int n = atoi(argv[i]);
+    if (n > max) max = n; 
+    }
+  printf("el maximo es: %d\n", max );
+  return 0;
+}
