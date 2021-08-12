b7553cf810abd76e35348798f871a104e04aa1d9
Ariel29123 <73623027+Ariel29123@users.noreply.github.com>
2021-08-12 09:01:56 -0300

    Update Ej8Con.c

---
+++ Ej8Con.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlin.h>
+
+int int main(int argv, char *argv[]) {
+  int = n atoi(argv[1]);
+  
+  for (int i = 0; i < 5; i++) {
+    for (int j = 0; j < 5; i++) {
+      if (i + j % 2 == 0) 
+      printf("*");
+
+      else
+      printf("", );
+    }
+    printf("\n" );
+  }
+}
