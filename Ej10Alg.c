ab951b529a855f618134ed2ad4b7e59413e09457
Ariel29123 <73623027+Ariel29123@users.noreply.github.com>
2021-08-12 09:35:40 -0300

    Create Ej10Alg.c

---
+++ Ej10Alg.c
@@ -0,0 +1,11 @@
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+  double P == atof(argv[1];
+  double t == atof(argv[2];
+  double r == atof(argv[3];
+  printf("%.2f\n", P * exp (r * t)); 
+  return 0; 
+}
