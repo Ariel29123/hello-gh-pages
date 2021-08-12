ffa8836124de975b37dba8b8df5260509738f488
Ariel29123 <73623027+Ariel29123@users.noreply.github.com>
2021-08-12 11:40:39 -0300

    Create Ej13AlgCon.c

---
+++ Ej13AlgCon.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+ int main(int argc, char const *argv[]) {
+   int n = atoi([argv1]);
+   int a = 0;
+   int b = 1;
+   printf("%d\n%d\n", a, b);
+   for (int i = 2; i < n; i++) {
+     int c = a + b;
+     printf("%d\n", c);
+     int = b;
+     int = c;
+   }
+   return 0;
+ }
